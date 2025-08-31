2f769d90e1db59c0829fe96b6d1de57430f02f3e
wuilmer carbajal <wcarbajal@outlook.com>
2025-08-30 23:38:02 -0500

    en la ficha del procedimiento

--- controllers/ficha.controllers.js
+++ controllers/ficha.controllers.js
@@ -114,8 +114,222 @@ const byIdRegistrarInputOutput = async ( req = request, res = response ) => {
   }
 };
 
+const getRiesgosByIdFicha = async ( req = request, res = response ) => {
+
+  const { id } = req.params;
+
+  try {
+    const riesgos = await prisma.riesgo.findMany( {
+      where: { fichaId: Number( id ) },
+    } );
+
+    if ( !riesgos ) {
+      return res.status( 404 ).json( {
+        ok: false,
+        msg: 'Ficha no encontrada'
+      } );
+    }
+
+    res.json( {
+      ok: true,
+      riesgos
+    } );
+
+  } catch ( error ) {
+    console.error( error );
+    res.status( 500 ).json( {
+      ok: false,
+      msg: 'Error al obtener Riesgos'
+    } );
+  }
+};
+
+const byIdFichaRegistrarRiesgos = async ( req = request, res = response ) => {
+
+  const { id } = req.params;
+  const fichaId = Number( id );
+  const { riesgos } = req.body;
+
+  try {
+
+    const ficha = await prisma.ficha.findUnique( { where: { id: fichaId } } );
+
+    if ( !ficha ) {
+      return res.status( 404 ).json( { ok: false, msg: 'Ficha no encontrada' } );
+    }
+
+    // 1. Obtener todos los registros actuales de la ficha
+    const existentes = await prisma.riesgo.findMany( {
+      where: { fichaId: Number( fichaId ) },
+    } );
+
+    // 2. Crear un Set de proveedores recibidos para fácil comparación
+    const riesgosRecibidos = new Set( riesgos.map( item => item.id ) );
+
+    // 3. Eliminar los que ya no están en el array recibido
+    const aEliminar = existentes.filter(
+      item => !riesgosRecibidos.has( item.id )
+    );
+    for ( const item of aEliminar ) {
+      await prisma.riesgo.delete( { where: { id: item.id } } );
+    }
+
+    // 4. Procesar cada item recibido: actualizar si existe, crear si no
+    for ( const item of riesgos ) {
+      const existente = existentes.find(
+        dbItem => dbItem.id === item.id
+      );
+      if ( existente ) {
+        await prisma.riesgo.update( {
+          where: { id: existente.id },
+          data: {
+            fichaId,
+            denominacion: item.denominacion,
+          },
+        } );
+      } else {
+
+        await prisma.riesgo.create( {
+          data: {
+            fichaId,
+            denominacion: item.denominacion,
+          },
+        } );
+      }
+    }
+
+    // 5. Obtener el estado final actualizado
+    const result = await prisma.riesgo.findMany( {
+      where: { fichaId: Number( fichaId ) },
+    } );
+
+    res.json( {
+      ok: true,
+      msg: 'Riesgos registrados correctamente',
+      result,
+    } );
+
+  } catch ( error ) {
+    console.error( error );
+    res.status( 500 ).json( {
+      ok: false,
+      msg: 'Error al registrar Riesgos',
+    } );
+  }
+};
+
+const byIdFichaRegistrarRegistros = async ( req = request, res = response ) => {
+
+  const { id } = req.params;
+  const fichaId = Number( id );
+  const { registros } = req.body;
+
+  try {
+
+    const ficha = await prisma.ficha.findUnique( { where: { id: fichaId } } );
+
+    if ( !ficha ) {
+      return res.status( 404 ).json( { ok: false, msg: 'Ficha no encontrada' } );
+    }
+
+    // 1. Obtener todos los registros actuales de la ficha
+    const existentes = await prisma.registro.findMany( {
+      where: { fichaId: Number( fichaId ) },
+    } );
+
+    // 2. Crear un Set de registros recibidos para fácil comparación
+    const registrosRecibidos = new Set( registros.map( item => item.id ) );
+
+    // 3. Eliminar los que ya no están en el array recibido
+    const aEliminar = existentes.filter(
+      item => !registrosRecibidos.has( item.id )
+    );
+    for ( const item of aEliminar ) {
+      await prisma.registro.delete( { where: { id: item.id } } );
+    }
+
+    // 4. Procesar cada item recibido: actualizar si existe, crear si no
+    for ( const item of registros ) {
+      const existente = existentes.find(
+        dbItem => dbItem.id === item.id
+      );
+      if ( existente ) {
+        await prisma.registro.update( {
+          where: { id: existente.id },
+          data: {
+            fichaId,
+            denominacion: item.denominacion,
+            tipoRegistro: item.tipoRegistro
+          },
+        } );
+      } else {
+
+        await prisma.registro.create( {
+          data: {
+            fichaId,
+            denominacion: item.denominacion,
+            tipoRegistro: item.tipoRegistro
+          },
+        } );
+      }
+    }
+
+    // 5. Obtener el estado final actualizado
+    const result = await prisma.riesgo.findMany( {
+      where: { fichaId: Number( fichaId ) },
+    } );
+
+    res.json( {
+      ok: true,
+      msg: 'Registros registrados correctamente',
+      result,
+    } );
+
+  } catch ( error ) {
+    console.error( error );
+    res.status( 500 ).json( {
+      ok: false,
+      msg: 'Error al registrar Riesgos',
+    } );
+  }
+};
+
+const getRegistrosByIdFicha = async ( req = request, res = response ) => {
+
+  const { id } = req.params;
+
+  try {
+    const registros = await prisma.registro.findMany( {
+      where: { fichaId: Number( id ) },
+    } );
+
+    if ( !registros ) {
+      return res.status( 404 ).json( {
+        ok: false,
+        msg: 'Ficha no encontrada'
+      } );
+    }
+
+    res.json( {
+      ok: true,
+      registros
+    } );
+
+  } catch ( error ) {
+    console.error( error );
+    res.status( 500 ).json( {
+      ok: false,
+      msg: 'Error al obtener Riesgos'
+    } );
+  }
+};
+
 
 module.exports = {
   getInputOutputById,
-  byIdRegistrarInputOutput
+  byIdRegistrarInputOutput,
+  getRiesgosByIdFicha,
+  byIdFichaRegistrarRiesgos,
+  getRegistrosByIdFicha,
+  byIdFichaRegistrarRegistros
 };
--- routes/ficha.routes.js
+++ routes/ficha.routes.js
@@ -1,7 +1,13 @@
 const { Router } = require( 'express' );
 const { check } = require( 'express-validator' );
 const { validarCampos } = require( '../middlewares/validar-campos' );
-const { getInputOutputById, byIdRegistrarInputOutput } = require( '../controllers/ficha.controllers' );
+const { getInputOutputById,
+  byIdRegistrarInputOutput,
+  getRiesgosByIdFicha,
+  byIdFichaRegistrarRiesgos,
+  getRegistrosByIdFicha,
+  byIdFichaRegistrarRegistros
+ } = require( '../controllers/ficha.controllers' );
 
 
 const router = Router();
@@ -17,10 +23,24 @@ router.get( '/:id/input-output', [
   validarCampos
 ], getInputOutputById );
 
+router.get( '/:id/riesgos', [  
+  validarCampos
+], getRiesgosByIdFicha );
 
+router.get( '/:id/registros', [  
+  validarCampos
+], getRegistrosByIdFicha );
 
 router.post( '/:id/registrar-io', [ 
   validarCampos
 ], byIdRegistrarInputOutput );
+
+router.post( '/:id/registrar-riesgos', [ 
+  validarCampos
+], byIdFichaRegistrarRiesgos );
+
+router.post( '/:id/registrar-registros', [ 
+  validarCampos
+], byIdFichaRegistrarRegistros );
 
 module.exports = router;
